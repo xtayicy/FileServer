5c0dbc820c23f3f12efdf7eb9c987b21d8de7850
harry <xtayicy@gmail.com>
2018-06-21 13:42:46 +0800

    文件服务器demo

--- src/main/java/harry/FileServer/util/MD5Util.java
+++ src/main/java/harry/FileServer/util/MD5Util.java
@@ -11,18 +11,20 @@ import org.junit.Test;
  *
  */
 public class MD5Util {
+	@Test
+	public void testEncode(){
+		String str = "zhangsan";
+		
+		System.out.println(MD5Util.encode(str.getBytes(), 0, str.length()));
+	}
+	
 	public static String encode(byte[] bytes,int start,int end){
 		try {
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
 			byte[] digest = md5.digest(bytes);
 			StringBuilder sb = new StringBuilder();
 			for(int i = 0;i < digest.length;i++){
-				int e = digest[i] & 0xFF;
-				if(e < 16){
-					sb.append(0);
-				}
-				
-				sb.append(Integer.toHexString(e));
+				sb.append(Integer.toHexString(digest[i] & 0xFF));
 			}
 			
 			return sb.toString();
