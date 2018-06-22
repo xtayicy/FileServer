64a5694ba55f352ce8733f02988e9e74d2625c15
harry <xtayicy@gmail.com>
2018-06-22 19:06:55 +0800

    文件服务器demo

--- src/main/java/harry/FileServer/util/MD5Util.java
+++ src/main/java/harry/FileServer/util/MD5Util.java
@@ -1,7 +1,14 @@
 package harry.FileServer.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+
+import org.junit.Test;
 
 /**
  * 
@@ -9,11 +16,36 @@ import java.security.NoSuchAlgorithmException;
  *
  */
 public class MD5Util {
+	@Test
+	public void testSign() throws FileNotFoundException{
+		System.out.println(sign(new FileInputStream(new File("sun.jpg"))));
+	}
+	
+	public static String sign(InputStream in){
+		try {
+			byte[] b = new byte[1024];
+			int len;
+			StringBuffer sb = new StringBuffer();
+			while((len = in.read(b)) != -1){
+				sb.append(encode(b,0,len));
+			}
+			
+			byte[] bytes = sb.toString().getBytes();
+			return encode(bytes,0,bytes.length);
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e.getMessage());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
 	public static String encode(byte[] bytes,int start,int end){
 		try {
 			MessageDigest md5 = MessageDigest.getInstance("MD5");
 			byte[] digest = md5.digest(bytes);
-			StringBuilder sb = new StringBuilder();
+			StringBuffer sb = new StringBuffer();
 			for(int i = 0;i < digest.length;i++){
 				sb.append(Integer.toHexString(digest[i] & 0xFF));
 			}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -9,18 +9,27 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
   <head>
     <base href="<%=basePath%>">
     
-    <title>My JSP 'index.jsp' starting page</title>
 	<meta http-equiv="pragma" content="no-cache">
 	<meta http-equiv="cache-control" content="no-cache">
-	<meta http-equiv="expires" content="0">    
-	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
-	<meta http-equiv="description" content="This is my page">
-	<!--
-	<link rel="stylesheet" type="text/css" href="styles.css">
-	-->
+	<meta http-equiv="expires" content="0">
+	<script type="text/javascript" src="${pageContext.request.contextPath }/js/jquery-1.7.2.min.js"></script>  
   </head>
   
   <body>
-    
+    <form onsubmit="return sumbit();" target="upload" method="post" enctype="multipart/form-data" action="${pageContext.request.contextPath }/upload">
+        <input type="file" name="file"/>
+        <script type="text/javascript">
+        	function sumbit(){
+        		if($("input[name='file']").val() == ''){
+        			alert("请先上传文件！");
+        			return false;
+        		}
+        		
+        		return true;
+        	}
+        </script>
+        <input type="submit" value="提交"/>
+    </form>
+    <iframe name="upload"></iframe>
   </body>
 </html>
